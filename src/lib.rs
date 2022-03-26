fa0c39da5c3460aaa3de0449cf6e2c0d7a03fca1
nesken7777 <51521915+nesken7777@users.noreply.github.com>
2022-03-26 10:31:53 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ pub mod yulib_memory;
 use crate::filter::*;
 use auls_memref2::CMemref;
 use std::cell::RefCell;
-use std::ffi::c_void;
 use std::ptr::null_mut;
 use windows_sys::Win32::Foundation::BOOL;
 use windows_sys::Win32::UI::WindowsAndMessaging::{PostMessageA, WM_COMMAND};
